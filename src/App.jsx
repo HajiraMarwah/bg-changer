4bf2254f98d39c7ca1d705f18bd17c2948ab47e3
Hajira Marwah <hajiranadia12@gmail.com>
2025-08-27 20:31:36 +0530

    added bg-changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,49 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import "./index.css";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import "./index.css"
+// import './App.css'
 
 function App() {
-  const [length, setLength] = useState(8);
-  const [numAllowed, setNumAllowed] = useState(false);
-  const [charAllowed, setCharAllowed] = useState(false);
-  const [password, setPassword] = useState("");
-  const [copyText, setCopyText] = useState("Copy");
-
-  const passwordRef = useRef(null);
-
-  const passwordGenerator = useCallback(() => {
-    let pass = "";
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-    if (numAllowed) str += "0123456789";
-    if (charAllowed) str += "!@#$%^&*()_+-=[]{}|;:',.<>/?`~";
-
-    for (let i = 0; i < length; i++) {
-      let char = Math.floor(Math.random() * str.length);
-      pass += str.charAt(char);
-    }
-
-    setPassword(pass);
-  }, [length, numAllowed, charAllowed, setPassword]);
-  const copyPasswordToClipboard = useCallback(() => {
-    passwordRef.current?.select();
-    window.navigator.clipboard.writeText(password);
-    setCopyText("Copied");
-    setTimeout(() => setCopyText("Copy"), 2000);
-  }, [password]);
-  useEffect(() => {
-    passwordGenerator();
-  }, [length, numAllowed, charAllowed, passwordGenerator]);
+   const [color, setColor] = useState("olive")
+ const colors = [
+    { name: "Olive", value: "olive", textColor: "white" },
+    { name: "Gray", value: "gray", textColor: "white" },
+    { name: "Yellow", value: "yellow", textColor: "black" },
+    { name: "Pink", value: "pink", textColor: "white" },
+    { name: "Purple", value: "purple", textColor: "white" },
+    { name: "Lavender", value: "lavender", textColor: "black" },
+    { name: "White", value: "white", textColor: "black" },
+    { name: "Black", value: "black", textColor: "white" },
+  ]
   return (
     <>
-      <div className="w-full max-w-md shadow-md mx-auto my-4 rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-        <h1 className="text-white text-center my-3">Password Generator</h1>
-        <div className="flex shadow overflow-hidden rounded-lg mb-4">
-          <input
-            className="outline-none w-full py-1 px-3"
-            value={password}
-            placeholder="Password"
-            ref={passwordRef}
-          />
-          <button
-            onClick={copyPasswordToClipboard}
-            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
-          >
-            {copyText}
-          </button>
-        </div>
-        <div className="flex text-sm gap-x-2">
-          <div className="flex text-center gap-x-1">
-            <input
-              type="range"
-              min={6}
-              max={100}
-              value={length}
-              onChange={(e) => setLength(e.target.value)}
-              className="cursor-pointer"
-            />
-            <label>Length:{length}</label>
-          </div>
-          <div className="flex text-center gap-x-1">
-            <input
-              type="checkbox"
-              defaultChecked={numAllowed}
-              id="numberInput"
-              onChange={() => setCharAllowed((prev) => !prev)}
-            />
-            <label>Numbers</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="characterInput"
-              defaultChecked={charAllowed}
-              onChange={() => setCharAllowed((prev) => !prev)}
-            />
-            <label>Characters</label>
-          </div>
-        </div>
+      <div className="w-full h-screen duration-200" style={{ backgroundColor: color }}>
+     <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="flex flex-wrap justify-center gap-3  shadow-lg bg-white px-3 py-2 rounded-3xl">
+        <button className='outline-none px-4 py-1 rounded-full shadow-xl text-white' style={{backgroundColor:"red"}} onClick={()=>setColor("red")}>Red</button>
+        <button className='outline-none px-4 py-1 rounded-full shadow-xl text-white' style={{backgroundColor:"green"}} onClick={()=>setColor("green")}>Green</button>
+        <button className='outline-none px-4 py-1 rounded-full shadow-xl text-white' style={{backgroundColor:"blue"}} onClick={()=>setColor("blue")}>Blue</button>
+        <button className='outline-none px-4 py-1 rounded-full shadow-xl text-white' style={{backgroundColor:"olive"}} onClick={()=>setColor("olive")}>Olive</button>
+        <button className='outline-none px-4 py-1 rounded-full shadow-xl text-white' style={{backgroundColor:"gray"}} onClick={()=>setColor("gray")}>Gray</button>
+
+        <button className='outline-none px-4 py-1 rounded-full shadow-xl text-black' style={{backgroundColor:"yellow"}} onClick={()=>setColor("yellow")}>Yellow</button>
+        <button className='outline-none px-4 py-1 rounded-full shadow-xl text-black' style={{backgroundColor:"pink"}} onClick={()=>setColor("pink")}>pink</button>
+        <button className='outline-none px-4 py-1 rounded-full shadow-xl text-white' style={{backgroundColor:"purple"}} onClick={()=>setColor("purple")}>purple</button>
+        <button className='outline-none px-4 py-1 rounded-full shadow-xl text-black' style={{backgroundColor:"lavender"}} onClick={()=>setColor("lavender")}>lavender</button>
+        <button className='outline-none px-4 py-1 rounded-full shadow-xl text-black' style={{backgroundColor:"white"}} onClick={()=>setColor("white")}>white</button>
+        <button className='outline-none px-4 py-1 rounded-full shadow-xl text-white' style={{backgroundColor:"black"}} onClick={()=>setColor("black")}>Black</button>
+
+
+
       </div>
+     </div>
+    </div>
+
     </>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
